35d4296240b1e131343ca45eb7e55d058086e898
Nikita <nikita_givchik@mail.ru>
2019-03-16 17:36:05 +0300

    Сделал валидацию на всех формах и в блоке Feedback переделал верстку инпутов

--- js/validate.js
+++ js/validate.js
@@ -1,11 +1,90 @@
-$('#feedback-form').validate({
+$('#information-form').validate({
+	errorClass: "invalid",
+	errorElement: "div",
 	rules: {
 		username: {
+			required: true,
+			minlength: 2,
+			maxlength: 15
+		},
+		phone: {
 			required: true
 		},
+		email: {
+			required: true,
+			email: true
+		}
 	},
 	messages: {
-		username: "Укажите имя",
-		minlength: JQuery.validator.format("Осталось символов: {0}")
+		username: {
+			required: "Введите ваше имя",
+			minlength: jQuery.validator.format("Минимум символов в имени: {0}"),
+			maxlength: jQuery.validator.format("Максимум символов в имени: {0}")
+		},
+		phone: {
+			required: "Введите ваш номер телефона"
+		},
+		email: {
+			required: "Укажите ваш email",
+			email: "Введите корректный email"
+		}
+	}
+});
+
+$('#feedback-form').validate({
+	errorClass: "invalid",
+	errorElement: "div",
+	rules: {
+		username: {
+			required: true,
+			minlength: 2,
+			maxlength: 15
+		},
+		phone: {
+			required: true
+		},
+		email: {
+			required: true,
+			email: true
+		}
+	},
+	messages: {
+		username: {
+			required: "Введите ваше имя",
+			minlength: jQuery.validator.format("Минимум символов в имени: {0}"),
+			maxlength: jQuery.validator.format("Максимум символов в имени: {0}")
+		},
+		phone: {
+			required: "Введите ваш номер телефона"
+		},
+		email: {
+			required: "Укажите ваш email",
+			email: "Введите корректный email"
+		}
+	}
+});
+
+$('#modal-form').validate({
+	errorClass: "invalid",
+	errorElement: "div",
+	rules: {
+		username: {
+			required: true,
+			minlength: 2,
+			maxlength: 15
+		},
+		phone: {
+			required: true
+		}
+	},
+	messages: {
+		username: {
+			required: "Введите ваше имя",
+			minlength: jQuery.validator.format("Минимум символов в имени: {0}"),
+			maxlength: jQuery.validator.format("Максимум символов в имени: {0}")
+		},
+		phone: {
+			required: "Введите ваш номер телефона"
+		}
 	}
 });
