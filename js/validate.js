df09127de607f2b193ccbec4baf94a54de881edb
Nikita <nikita_givchik@mail.ru>
2019-03-25 14:35:23 +0300

    Добавил отправку формы на почту

--- js/validate.js
+++ js/validate.js
@@ -1,36 +1,3 @@
-$('#information-form').validate({
-	errorClass: "invalid",
-	errorElement: "div",
-	rules: {
-		userName: {
-			required: true,
-			minlength: 2,
-			maxlength: 15
-		},
-		userPhone: {
-			required: true
-		},
-		userEmail: {
-			required: true,
-			email: true
-		}
-	},
-	messages: {
-		userName: {
-			required: "Введите ваше имя",
-			minlength: jQuery.validator.format("Минимум символов в имени: {0}"),
-			maxlength: jQuery.validator.format("Максимум символов в имени: {0}")
-		},
-		userPhone: {
-			required: "Введите ваш номер телефона"
-		},
-		userEmail: {
-			required: "Укажите ваш email",
-			email: "Введите корректный email"
-		}
-	}
-});
-
 $('#feedback-form').validate({
 	errorClass: "invalid",
 	errorElement: "div",
@@ -62,15 +29,61 @@ $('#feedback-form').validate({
 			email: "Введите корректный email"
 		}
 	},
-	submitHandler: function () {
+	submitHandler: function (form) {
 		$.ajax({
-			url: 'php/mail.php',
+			url: 'phpmailer/sendFeedback.php',
 			type: 'POST',
-			data: $(this).serialize(),
+			data: $(form).serialize(),
 			success: function (data) {
 				console.log(data);
 				alert(data + ', Ваша форма отправлена! Мы скоро перезвоним!');
 				$("#feedback-form").trigger('reset');
+			}
+		});
+		return false;
+	}
+});
+
+$('#information-form').validate({
+	errorClass: "invalid",
+	errorElement: "div",
+	rules: {
+		userName: {
+			required: true,
+			minlength: 2,
+			maxlength: 15
+		},
+		userPhone: {
+			required: true
+		},
+		userEmail: {
+			required: true,
+			email: true
+		}
+	},
+	messages: {
+		userName: {
+			required: "Введите ваше имя",
+			minlength: jQuery.validator.format("Минимум символов в имени: {0}"),
+			maxlength: jQuery.validator.format("Максимум символов в имени: {0}")
+		},
+		userPhone: {
+			required: "Введите ваш номер телефона"
+		},
+		userEmail: {
+			required: "Укажите ваш email",
+			email: "Введите корректный email"
+		}
+	},
+	submitHandler: function (form) {
+		$.ajax({
+			url: 'phpmailer/sendInformation.php',
+			type: 'POST',
+			data: $(form).serialize(),
+			success: function (data) {
+				console.log(data);
+				alert(data + ', Ваша форма отправлена! Мы скоро перезвоним!');
+				$("#information-form").trigger('reset');
 			}
 		});
 		return false;
@@ -99,5 +112,18 @@ $('#modal-form').validate({
 		userPhone: {
 			required: "Введите ваш номер телефона"
 		}
+	},
+	submitHandler: function (form) {
+		$.ajax({
+			url: 'phpmailer/sendModal.php',
+			type: 'POST',
+			data: $(form).serialize(),
+			success: function (data) {
+				console.log(data);
+				alert(data + ', Ваша форма отправлена! Мы скоро перезвоним!');
+				$("#modal-form").trigger('reset');
+			}
+		});
+		return false;
 	}
 });
---
+++ phpmailer/sendModal.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'class.phpmailer.php';
+require 'class.smtp.php';
+
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
+$userEmail= $_POST['userEmail'];
+
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->isSMTP(); 
+$mail->Host = 'smtp.mail.ru';  
+$mail->SMTPAuth = true;
+$mail->Username = 'nikita_givchik2@mail.ru';
+$mail->Password = '10121997deep123';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('nikita_givchik2@mail.ru', 'Ремонт квартир "ОУМАЙ"');
+$mail->addAddress('nikita_givchik@mail.ru');
+
+$mail->isHTML(true); 
+
+$mail->Subject = "Новая заявка с сайта";
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $userName . ' <br>
+	Телефон: ' . $userPhone . ' <br>
+	Email: '. $userEmail . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+// Результат
+if(!$mail->send()) {
+    echo "Error";
+} else {
+    echo $userName;
+}
+
+?>
